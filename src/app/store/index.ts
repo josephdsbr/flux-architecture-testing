7473d76c5c88bf1c39704ff0eecd8af5bd966420
josephdsbr <josephdsbr@gmail.com>
2020-09-08 19:19:45 -0300

    Ajustando o nome do CartReducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -7,15 +7,14 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { CartState } from '../models/cart.model';
-import { cartReducer } from './modules/cart/cart.reducer';
+import { CartReducer } from './modules/cart/cart.reducer';
 
 export interface AppState {
   cart: CartState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  cart: cartReducer
+  cart: CartReducer
 };
-
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
