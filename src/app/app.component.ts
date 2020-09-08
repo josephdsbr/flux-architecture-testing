4642f7a06d68cf93c785031e4e9e9190670fc886
josephdsbr <josephdsbr@gmail.com>
2020-09-08 09:37:22 -0300

    Ajustando header para mostrar informações da quantidade a ser comprada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import es from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'flux-architecture-test';
+
+  ngOnInit(): void {
+    registerLocaleData(es);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import {LOCALE_ID} from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeBR from '@angular/common/locales/es-BR';
+registerLocaleData(localeBR, 'br');
 
 @NgModule({
   declarations: [
@@ -22,7 +26,10 @@ import { environment } from '../environments/environment';
     }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'br'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
